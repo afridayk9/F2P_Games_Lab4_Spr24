d3a62b62d419612b9851b4cd8622c6579873b5b6
afridayk9 <99346691+afridayk9@users.noreply.github.com>
2024-02-16 09:08:44 -0500

    Add project files.

---
+++ F2P_Games_Lab4_Spr24/Data/GameModel.cs
@@ -0,0 +1,20 @@
+﻿namespace F2P_Games_Lab4_Spr24.Data;
+
+
+
+public class GameModel
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public string thumbnail { get; set; }
+    public string short_description { get; set; }
+    public string game_url { get; set; }
+    public string genre { get; set; }
+    public string platform { get; set; }
+    public string publisher { get; set; }
+    public string developer { get; set; }
+    public string release_date { get; set; }
+    public string freetogame_profile_url { get; set; }
+}
+
+
---
+++ F2P_Games_Lab4_Spr24/Program.cs
@@ -0,0 +1,31 @@
+using F2P_Games_Lab4_Spr24.Data;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
